efb81c6a1e4cf3a45fb04c1ccd91fd2f32be601a
Peter Massaro <petermassaro@Peters-Air.home>
2018-05-12 11:00:36 -0400

    Fixed send SMS

--- app/sendSMS.py
+++ app/sendSMS.py
@@ -4,7 +4,10 @@ from threading import Thread
 
 
 def sendSMS(recipientNumber, messageContent):
-	client = Client(current_app.config['TWILIO_ACCOUNT_SID'], app.config['TWILIO_AUTH_TOKEN'])
+	client = Client(
+		current_app.config['TWILIO_ACCOUNT_SID'],
+		current_app.config['TWILIO_AUTH_TOKEN']
+		)
 	message = client.api.account.messages.create(
 		to='+1{}'.format(recipientNumber),
 		from_=current_app.config['TWILIO_NUMBER'],
